056083cf9035e5b8cf160a2297e04adbd8f60c02
Androgymo <androgymo@gmail.com>
2018-04-02 12:32:55 -0400

    additional changed to css

---
+++ script.js
@@ -0,0 +1,12 @@
+//Grab the search query when the user submits the form.
+
+$(function(){
+	const form = document.querySelector(".search");
+	console.log(form);
+
+})
+
+
+
+//Send this query to Wikipedia.
+//Display the results on the page.
