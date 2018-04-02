e061b8f08a803f9c7f5e0e7634c8d14ee3cd5973
Androgymo <androgymo@gmail.com>
2018-04-02 15:02:44 -0400

    adding scripts but not working; check with Derek

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 //Grab the search query when the user submits the form.
 
 $(function(){
-	var search = $(".search").val();
-	console.log(search);
+	$("#search").click(function(){
+		//the value entered into the search box
+		var keyword = $(".keyword").val();
+		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ keyword +"&format=json&callback=?";
 
+		$.ajax({
+	        type: "GET",
+	        url: "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ keyword +"&format=json&callback=?",
+	        contentType: "application/json; charset=utf-8",
+	        async: false,
+	        dataType: "jsonp",
+	        success: function (data, textStatus, jqXHR) {
+	            return $("#results").html(data);
+	        },
+	        error: function (errorMessage) {
+	        	console.log(ERROR);
+	        }
+
+		})
+	})
 })
-
 
 
 //Send this query to Wikipedia.
