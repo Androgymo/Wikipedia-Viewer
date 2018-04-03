8cfbc469edc198001757dd427286d654a5782c15
Androgymo <androgymo@gmail.com>
2018-04-02 20:32:23 -0400

    fixing ajax request

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 
 $(function(){
 	$("#search").click(function(){
-		//the value entered into the search box
-		var keyword = $(".keyword").val();
-		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ keyword +"&format=json&callback=?";
+		event.preventDefault(); //Prevents the page from refreshing
+		var keyword = $(".keyword").val(); //stores the value entered into the search box
 
 		$.ajax({
 	        type: "GET",
 	        url: "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ keyword +"&format=json&callback=?",
 	        contentType: "application/json; charset=utf-8",
 	        async: false,
-	        dataType: "jsonp",
+	        dataType: "json",
 	        success: function (data, textStatus, jqXHR) {
-	            return $("#results").html(data);
+	        	var obj = JSON.stringify(data);
+	        	var info = JSON.parse(data);
+	            $(".results").html(info);
 	        },
 	        error: function (errorMessage) {
 	        	console.log(ERROR);
